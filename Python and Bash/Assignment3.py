394d3bebd95c5d3ce04025e897dded94ff3cbd7d
Sumukh <sumukhgoutam0804@gmail.com>
2026-02-03 14:27:37 +0530

    Python Assignment

---
+++ Python and Bash/Assignment3.py
@@ -0,0 +1,9 @@
+file = open("sample.txt", "w")
+
+file.write("Hello!\n")
+file.write("This file is created using Python.\n")
+file.write("We are learning file handling.")
+
+file.close()
+
+print("Content written to file successfully.")
---
+++ Python and Bash/Assignment4.py
@@ -0,0 +1,6 @@
+with open("sample.txt", "w") as file:
+    file.write("Hello!\n")
+    file.write("This file is created using Python.\n")
+    file.write("We are learning file handling.")
+
+print("Content written to file successfully.")
